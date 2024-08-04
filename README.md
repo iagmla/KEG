f9131cb422d60e7939f8904b13c481df57db44aa
Karl Zander <karl.c.zander@gmail.com>
2024-08-04 09:13:00 -0700

    KEG complete

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # KEG Cipher
 
-*** Use KEG-R
+Kolor Encryption Gate is a playing card cipher designed to emulate motions of a game.  It is a hand-held, non-periodic rotor machine.  It's strength is the length of the deck (52 factorial).
 
-Karl's Encryption Gate is a playing card cipher designed to emulate motions of a game.  It is a hand-held, non-periodic rotor machine.  It's strength is the length of the deck (52 factorial).
+# KEG-R
+
+Karl's Encryption Gate (Revised) is a playing card cipher designed to emulate motions of a game.  It is a hand-held, non-periodic rotor machine.  It's strength is the length of the deck (52 factorial).
--- keg.py
+++ keg.py
@@ -1,47 +1,47 @@
 ''' KEG Cipher '''
-''' Karl's Encryption Gate '''
+''' Kolor Encryption Gate '''
 ''' by KryptoMagick (Karl Zander) '''
 
 class KEG:
     def __init__(self):
         self.values = {}
         self.discard = []
-        suit = 0
-        value = 0
+        color = 0
         for x in range(52):
-            if x % 13 == 0 and x != 0:
-                suit += 1
-            self.values[x] = (value, suit)
-            value = (value + 1) % 26
+            if x % 26 == 0 and x != 0:
+                color += 1
+            self.values[x] = (x % 26, color, x)
 
     def key(self, key):
         # Set the deck to equal numbers 0-51
         self.deck = list(key)
-        # Set the gate suit to be the suit
+        # Set the gate color to be the color
         # Of the first card in the deck
-        self.suit = self.values[self.deck[0]][1]
+        self.gate_color = self.values[self.deck[0]][1]
 
     def step(self):
-        values = self.values[self.deck[1]]
-        step_value = values[0]
-        # If the stepping card equals the gate suit
-        # Move the dicard pile to the rear of the encryption pile
-        if values[1] == self.suit and len(self.discard) != 0:
+        step_card_index = 1
+        crypt_card_index = 0
+        card_values = self.values[self.deck[step_card_index]]
+        step_value = card_values[2]
+        card_color = card_values[1]
+        if card_color == self.gate_color and len(self.discard) != 0:
             for x in range(len(self.discard)):
                 self.deck.append(self.discard.pop(0))
-            # Discard the stepping card
-            self.discard.append(self.deck.pop(1))
-        else:
-            # Discard the stepping card
-            self.discard.append(self.deck.pop(1))
+        # Discard the stepping card
+        self.discard.append(self.deck.pop(step_card_index))
         # Step the encryption deck by removing the first card
         # Place it in the rear
-        self.deck.append(self.deck.pop(0))
+        self.deck.append(self.deck.pop(crypt_card_index))
         # Step the encryption deck by the stepping value
         # Removing the first card and place it in the rear
         # Repeat until the stepping value has been reached
         for x in range(step_value):
-            self.deck.append(self.deck.pop(0))
+            self.deck.append(self.deck.pop(crypt_card_index))
+
+    def key_scheduler(self):
+        for x in range(52):
+            self.step()
 
     def encrypt_letter(self, letter):
         # Step the encryption pile
@@ -49,6 +49,7 @@ class KEG:
         # Convert the letter to number 0-25
         num = ord(letter) - 65
         # Encipher the letter
+        #print(len(self.deck), len(self.discard))
         return chr(((num + self.values[self.deck[0]][0]) % 26) + 65)
 
     def decrypt_letter(self, letter):
@@ -57,17 +58,22 @@ class KEG:
         # Convert the letter to number 0-25
         num = ord(letter) - 65
         # Decipher the letter
+        #print(len(self.deck), len(self.discard))
         return chr(((num - self.values[self.deck[0]][0]) % 26) + 65)
 
-    def encrypt(self, letters):
+    def encrypt(self, letters, ksa=False):
         ctxt = []
+        if ksa:
+            self.key_scheduler()
         for x in range(len(letters)):
             letter = self.encrypt_letter(letters[x])
             ctxt.append(letter)
         return "".join(ctxt)
 
-    def decrypt(self, letters):
+    def decrypt(self, letters, ksa=False):
         ptxt = []
+        if ksa:
+            self.key_scheduler()
         for x in range(len(letters)):
             letter = self.decrypt_letter(letters[x])
             ptxt.append(letter)
